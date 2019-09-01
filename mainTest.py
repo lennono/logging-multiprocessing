3236e35b06337dea407425d225d56be7ed275a17
lennono <owenlennon3@gmail.com>
2019-09-01 21:46:47 +0100

    Added some comments - More what to next

--- mainTest.py
+++ mainTest.py
@@ -1,11 +1,11 @@
 from exception import CustomHandling
 from multiprocessing import Queue
-queue = Queue()
+queue = Queue()  # Need  a better solution here
 
 
 @CustomHandling(queue)
 def zero_divide():
-    1 / 0
+    1 / 0  # throwing deliberate error
 
 
 if __name__ == '__main__':
